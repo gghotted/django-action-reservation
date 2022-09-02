7dc87094cfc0b8b65526fdeedc4ea1f5c5b7a5e6
gghotted <gghotted2@naver.com>
2022-09-03 02:00:54 +0900

    add install_requires

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(
     name='django_action_reservation',
     version='0.0.0',
     packages=['django_action_reservation'],
+    install_requires=[
+        'django>=3.5',
+        'django-appconf>=1.0.0',
+    ]
 )
