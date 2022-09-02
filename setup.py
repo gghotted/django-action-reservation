2629763e165cdc88145cfd4a1c9d5bfc6eec20b2
gghotted <gghotted2@naver.com>
2022-09-03 03:11:23 +0900

    packages update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='django_action_reservation',
     version='0.0.0',
-    packages=['django_action_reservation'],
+    packages=find_packages(exclude=['demo*']),
     install_requires=[
         'django>=3.5',
         'django-appconf>=1.0.0',
