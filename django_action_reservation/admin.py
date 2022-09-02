a0975fceec5f0f71964df021ed98c39a145fe25b
gghotted <gghotted2@naver.com>
2022-09-03 02:09:43 +0900

    add order & filter

--- django_action_reservation/admin.py
+++ django_action_reservation/admin.py
@@ -51,15 +51,23 @@ class ActionReservationAdmin(admin.ModelAdmin):
         'error_message',
         'state',
     )
+    list_filter = (
+        'action',
+        'log__state',
+    )
 
+    @admin.display(ordering='log__started')
     def started(self, obj):
         return localize(localtime(obj.log.started)) if obj.log else None
 
+    @admin.display(ordering='log__ended')
     def ended(self, obj):
         return localize(localtime(obj.log.ended)) if obj.log else None
 
+    @admin.display(ordering='log__error_message')
     def error_message(self, obj):
         return obj.log.error_message if obj.log else None
     
+    @admin.display(ordering='log__state')
     def state(self, obj):
         return obj.log.display_state if obj.log else None
